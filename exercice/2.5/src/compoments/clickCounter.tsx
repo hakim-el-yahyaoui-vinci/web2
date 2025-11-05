0afb1220cb9b50a5310a05e2f7663e997f309569
Hakim El Yahyaoui <12345678+hakim-el-yahyaoui-vinci@users.noreply.github.com>
2025-11-05 12:21:33 +0100

    new:ex2.5-destructuring

--- exercice/2.5/src/compoments/clickCounter.tsx
+++ exercice/2.5/src/compoments/clickCounter.tsx
@@ -1,13 +1,22 @@
 import { useState } from "react";
 
-export function ClickCounter() {
+type ClickCounterProps = {
+  title: string;
+  message: string;
+};
+
+export function ClickCounter({ title, message }: ClickCounterProps) {
   const [count, setCount] = useState(0);
 
   return (
     <div className="card">
+      <h2>{title}</h2>
+      
       <button onClick={() => setCount((c) => c + 1)}>
         count is {count}
       </button>
+
+       {count >= 10 && <p>{message}</p>}
     </div>
   );
 }
